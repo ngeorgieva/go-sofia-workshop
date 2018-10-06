79c40076ee30ed6c5961bb92a9f54f58590da485
Nataliya Georgieva <ngeorgieva@axway.com>
2018-10-06 16:27:59 +0300

    added Dockerfile & Makefile

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Stage 1. Build the binary
+FROM golang:1.11
+
+# add a non-privileged user
+RUN useradd -u 10001 myapp
+
+RUN mkdir -p /go/src/github.com/ngeorgieva/go-sofia-workshop
+ADD . /go/src/github.com/ngeorgieva/go-sofia-workshop
+WORKDIR /go/src/github.com/ngeorgieva/go-sofia-workshop
+
+# build the binary with go build
+RUN go get ./... && CGO_ENABLED=0 go build \
+	-o bin/go-sofia-workshop github.com/ngeorgieva/go-sofia-workshop/cmd/go-sofia-workshop
+
+# Stage 2. Run the binary
+FROM scratch
+
+ENV PORT 8080
+ENV DIAG_PORT 8585
+
+COPY --from=0 /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+
+COPY --from=0 /etc/passwd /etc/passwd
+USER myapp
+
+COPY --from=0 /go/src/github.com/ngeorgieva/go-sofia-workshop/bin/go-sofia-workshop /go-sofia-workshop
+EXPOSE $PORT
+EXPOSE $DIAG_PORT
+
+CMD ["/go-sofia-workshop"]
--- cmd/go-sofia-workshop/main.go
+++ cmd/go-sofia-workshop/main.go
@@ -1,34 +1,99 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/ngeorgieva/go-sofia-workshop/internal/diagnostics"
 )
 
+type serverConf struct {
+	port   string
+	router http.Handler
+	name   string
+}
+
 func main() {
-	log.Print("Hello, World!")
+	log.Print("Starting the application...")
+
+	blPort := os.Getenv("PORT")
+	if len(blPort) == 0 {
+		log.Fatal("The application port should be set")
+	}
+
+	diagPort := os.Getenv("DIAG_PORT")
+	if len(diagPort) == 0 {
+		log.Fatal("The application port should be set")
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 
-	go func() {
-		err := http.ListenAndServe(":8080", router)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-}
-
+	possibleErrors := make(chan error, 2)
 	diagnostics := diagnostics.NewDiagnostics()
-	err := http.ListenAndServe(":8585", diagnostics)
-	if err != nil {
-		log.Fatal(err)
+
+	configurations := []serverConf{
+		{
+			port:   blPort,
+			router: router,
+			name:   "application server",
+		},
+		{
+			port:   diagPort,
+			router: diagnostics,
+			name:   "diagnostics router",
+		},
 	}
 
+	servers := make([]*http.Server, 2)
+
+	for i, c := range configurations {
+		go func(conf serverConf, i int) {
+			log.Printf("The %s is preparing to handle connections...", conf.name)
+			servers[i] = &http.Server{
+				Addr:    ":" + conf.port,
+				Handler: conf.router,
+			}
+			err := servers[i].ListenAndServe()
+			if err != nil {
+				possibleErrors <- err
+			}
+		}(c, i)
+	}
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-possibleErrors:
+		log.Printf("Got an error: %v", err)
+	case sig := <-interrupt:
+		log.Printf("Received the signal %v", sig)
+
+	}
+
+	for _, s := range servers {
+		timeout := 5 * time.Second
+		log.Printf("Shutdown with timeout: %s", timeout)
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		err := s.Shutdown(ctx)
+		if err != nil {
+			fmt.Println(err)
+		}
+		log.Printf("Server gracefully stopped")
+	}
+
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
+	log.Print("The hello handler was called.")
 	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
